5cbda9d62006e1b708bee7e5b0a12a2a01d0ee14
Vieri Harney <vieriharney@gmail.com>
2020-08-15 00:49:23 +0700

    Moving Code of Bottom Sheets

--- patungkuy/lib/shared/appbar.dart
+++ patungkuy/lib/shared/appbar.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:patungkuy/screens/home/add_order.dart';
+import 'package:patungkuy/services/auth.dart';
+import 'package:patungkuy/shared/appbar.dart';
+import 'package:provider/provider.dart';
+import 'package:patungkuy/services/database.dart';
+import 'package:patungkuy/models/order.dart';
+import 'package:patungkuy/screens/authenticate/authenticate.dart';
+import 'package:patungkuy/shared/drawer.dart';
+import 'package:patungkuy/shared/appbar.dart';
 
 class AppbarCustom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    void _showSettingsPanel() {
+    void _addOrderPanel() {
       showModalBottomSheet(
           context: context,
           builder: (context) {
             return Container(
-              child: Text('bottom sheet'),
+              child: AddOrder(),
             );
           });
     }
@@ -20,8 +29,8 @@ class AppbarCustom extends StatelessWidget {
       actions: <Widget>[
         FlatButton.icon(
           icon: Icon(Icons.add),
-          label: Text('add order'),
-          onPressed: () => _showSettingsPanel(),
+          label: Text('ADD ORDER'),
+          onPressed: () => _addOrderPanel(),
         )
       ],
       bottom: PreferredSize(
