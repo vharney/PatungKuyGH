18ff7dcf8a499ac23828322394b1944b9d6ad312
reyvb24 <61707645+reyvb24@users.noreply.github.com>
2020-08-15 12:09:48 +0700

    navbar keren

--- patungkuy/lib/screens/home/mycart.dart
+++ patungkuy/lib/screens/home/mycart.dart
@@ -3,6 +3,7 @@ import 'package:patungkuy/models/temp_order.dart';
 import 'package:patungkuy/shared/appbar.dart';
 import 'package:patungkuy/shared/drawer.dart';
 import 'package:patungkuy/shared/constants.dart';
+import 'package:patungkuy/screens/home/add_order.dart';
 
 class MyCart extends StatefulWidget {
   List<TempOrder> listOfTempOrders;
@@ -13,8 +14,31 @@ class MyCart extends StatefulWidget {
 
 class _MyCartState extends State<MyCart> {
   @override
+  void _addOrderPanel() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            child: AddOrder(),
+          );
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+        backgroundColor: Colors.blue[300],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('ADD ORDER'),
+            onPressed: () => _addOrderPanel(),
+          )
+        ],
+      ),
+      backgroundColor: Colors.blue[100],
       drawer: DrawerCustom(email: userEmail),
     );
   }
